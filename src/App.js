bf2aced72c2afa98bb047fc02f4af1c4b0a63ad6
Werefox <jack@wrightus.com>
2022-06-07 16:51:53 -0400

    Added the color block component to the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
+import ColorBlock from './ColorBlock';
 
 function App(){
-    let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+    let [ colors, setColors ] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+
+    let colorMap = colors.map((color, i) => {
+      return <ColorBlock color={color} key={i}/>
+    })
+
     return (
         <div className="App">
-
+          {colorMap}
         </div>
     )
 }
